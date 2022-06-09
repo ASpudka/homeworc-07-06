6f8b5d8e2de98fff8e5763a77887a42d3ddd932f
Aleksandra <aspudka@gmail.com>
2022-06-09 09:04:09 +0300

    Inition commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const getMagicNamber = function(userNumber = 5, magicNumber = 10){
+  let userNumber = Number(prompt('Think of a number from 1 to 10 and write it down here'));
+  if (userNumber === !isNaN(Number)||userNumber === ''||userNumber === null){
+    console.log ('It is not a number. Try again');
+  } 
+}
